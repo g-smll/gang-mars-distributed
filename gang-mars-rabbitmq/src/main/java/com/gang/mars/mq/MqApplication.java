b678f96abd90ce132d5dbe15c658462b050e0766
gang.chen <chengangyunnan@qq.com>
2021-03-31 10:09:09 +0800

    加入MQ应用

--- gang-mars-rabbitmq/src/main/java/com/gang/mars/mq/MqApplication.java
+++ gang-mars-rabbitmq/src/main/java/com/gang/mars/mq/MqApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author gang.chen
- * @description
+ * @description 消息应用
  * @time 2021/3/31 8:25
  */
 @SpringBootApplication
